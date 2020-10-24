735c434d593e7de5d45fb1e29617bfb43e9625d7
meloegel <meloegel@gmail.com>
2020-10-24 14:57:19 -0400

    updated to single img style

--- src/App.js
+++ src/App.js
@@ -13,19 +13,17 @@ class App extends Component {
         <div className="App-header">
           <div className='welcome'>
             <h1>Cube Timer</h1>
-            <p>To start and the cube timer, Press the Spacebar or click Start/Stop</p>
-            <p>To reset the timer, press 'b' or click Reset</p>
-            <p>To save a time, press 'v' or click Save Time</p>
-            <p>To clear saved a times, press 'q' or click Reset Saved Times</p>
-            <p>To get a new scramble algorithm, press 'n' or click New Scramble</p>
+            <p>To start and the cube timer, Press the <strong>Spacebar</strong> or click Start/Stop</p>
+            <p>To reset the timer, press <strong>'b'</strong> or click Reset</p>
+            <p>To save a time, press <strong>'v'</strong> or click Save Time</p>
+            <p>To clear saved a times, press <strong>'q'</strong> or click Reset Saved Times</p>
+            <p>To get a new scramble algorithm, press <strong>'n'</strong> or click New Scramble</p>
             <div className='scrambleDiv'>
               <Scrambler />
             </div>
-            <p>This scrambler is not WCA certified, it was created utilizing Javascripts random function and assigning the numbers to a move.</p>
+            <p id='cert'>**This scrambler is not WCA certified, it was created utilizing Javascripts random function and assigning the numbers to a move.**</p>
+            <Stopwatch />
           </div>
-        </div>
-        <div className='App-footer'>
-          <Stopwatch />
         </div>
       </div>
     );
